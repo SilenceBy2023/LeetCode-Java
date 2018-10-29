4f9f7a2fdaabf2577a13183d37e93a23f4ba944f
xiong <1095939843@qq.com>
2018-10-29 18:22:48 +0800

    添加3SumClosest算法

--- src/leetcode/ThreeSumClosest.java
+++ src/leetcode/ThreeSumClosest.java
@@ -11,14 +11,34 @@ public class ThreeSumClosest {
      * Given array nums = [-1, 2, 1, -4], and target = 1.
      *
      * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+     *
+     * 思路：
+     * 首先针对数组进行排序，先计算两个数值的和，然后再和target进行判断
+     * @param nums: Give an array numbers of n integer
+     * @param target : An integer
+     * @return : return the sum of the three integers, the sum closest target.
      */
     public int threeSumClosest(int[] nums, int target) {
-
-        Arrays.sort(nums);
-        int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            break;
+        if (nums == null || nums.length < 3) {
+            return -1;
         }
-        return res;
+        Arrays.sort(nums);
+        int bestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - sum) < Math.abs(target - bestSum)) {
+                    bestSum = sum;
+                }
+                if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        return bestSum;
     }
 }
--- test/leetcode/ThreeSumClosestTest.java
+++ test/leetcode/ThreeSumClosestTest.java
@@ -8,5 +8,11 @@ public class ThreeSumClosestTest {
 
     @Test
     public void threeSumClosest() {
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        ThreeSumClosest threeSumClosest = new ThreeSumClosest();
+        int res = threeSumClosest.threeSumClosest(nums, target);
+
+        System.out.println(res);
     }
 }
