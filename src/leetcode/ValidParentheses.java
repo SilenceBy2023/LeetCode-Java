3d3d92f86bfc5a3c9638570bafb00f3b7958ba00
xiong <1095939843@qq.com>
2018-10-30 11:16:15 +0800

    添加ValidParentheses算法二

--- src/leetcode/ValidParentheses.java
+++ src/leetcode/ValidParentheses.java
@@ -14,29 +14,53 @@ public class ValidParentheses {
      * Note that an empty string is also considered valid.
      */
     public boolean isValid(String s) {
-        if (s.length() == 0) {
-            return true;
-        }
-        Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case ')':
-                    if (stack.isEmpty() || !stack.pop().equals('('))
-                        return false;
+        //方法一
+//        if (s.length() == 0) {
+//            return true;
+//        }
+//        Stack<Character> stack = new Stack<Character>();
+//        for (int i = 0; i < s.length(); i++) {
+//            switch (s.charAt(i)) {
+//                case ')':
+//                    if (stack.isEmpty() || !stack.pop().equals('('))
+//                        return false;
+//                    break;
+//                case '}':
+//                    if (stack.isEmpty() || !stack.pop().equals('{'))
+//                        return false;
+//                    break;
+//                case ']':
+//                    if (stack.isEmpty() || !stack.pop().equals('['))
+//                        return false;
+//                    break;
+//                default:
+//                    stack.push(s.charAt(i));
+//                    break;
+//            }
+//        }
+//        return stack.isEmpty();
+
+        //方法二
+        char[] stack = new char[s.length()];
+        int head = 0;
+        for(char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack[head++] = c;
                     break;
                 case '}':
-                    if (stack.isEmpty() || !stack.pop().equals('{'))
-                        return false;
+                    if (head == 0 || stack[--head] != '{') return false;
+                    break;
+                case ')':
+                    if (head == 0 || stack[--head] != '(') return false;
                     break;
                 case ']':
-                    if (stack.isEmpty() || !stack.pop().equals('['))
-                        return false;
-                    break;
-                default:
-                    stack.push(s.charAt(i));
+                    if (head == 0 || stack[--head] != '[') return false;
                     break;
             }
         }
-        return stack.isEmpty();
+        return head == 0;
     }
 }
